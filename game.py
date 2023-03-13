41ee4196cc6e9ebdd6acb26132c0071d56f0d20f
olenamysyshyn <mysyshyn.pn@ucu.edu.ua>
2023-03-13 17:36:44 +0200

    for task 6

--- game.py
+++ game.py
@@ -1,1 +1,199 @@
+"""
+Module contains classes for the game "Блукачка по Львову"
+"""
+class Street:
+    """
+    class Street for Lviv streets
+    """
+    def __init__(self, name):
+        self.name = name
+        self.description = None
+        self.linked_streets = {}
+        self.character = None
+        self.item = None
 
+    def set_description(self, description):
+        """
+        Methods sets the description of the street
+        """
+        self.description = description
+
+    def get_description(self):
+        """
+        return the description of the exact street
+        """
+        return self.description
+
+    def get_name(self):
+        """
+        return the name
+        """
+        return self.name
+
+    def describe(self):
+        """
+        method for description
+        """
+        print(self.get_description())
+
+    def link_street(self, room_to_link, direction):
+        """
+        method links the direction to another street
+        """
+        self.linked_streets[direction] = room_to_link
+
+    def get_details(self):
+        """
+        method for details of the street
+        """
+        print(f"Ви знаходитесь на вулиці {self.name}.")
+        print(self.get_description())
+        for direction, street in self.linked_streets.items():
+            print(f"Вулиця {street.get_name()} знаходитися, як йти на {direction}.")
+        if self.character:
+            print(f"Ви можете бачити {self.character.get_name()} тут.")
+        if self.item:
+            print(f"Також є {self.item.name} тут.")
+
+    def set_character(self, character):
+        """
+        method sets character
+        """
+        self.character = character
+
+    def get_character(self):
+        """
+        for getting character
+        """
+        return self.character
+
+    def set_item(self, item):
+        """
+        sets item
+        """
+        self.item = item
+
+    def get_item(self):
+        """
+        for getting item
+        """
+        return self.item
+
+    def move(self, direction):
+        """
+        methods for making a move
+        """
+        if direction in self.linked_streets:
+            return self.linked_streets[direction]
+        print('Цей шлях не є коректним')
+        return self
+
+class Character:
+    """
+    class Character for defining a hero
+    """
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.conversation = None
+
+    def describe(self):
+        """
+        methos prints description
+        """
+        print(f"{self.name} тут!!!")
+        print(self.description)
+
+    def set_conversation(self, conversation):
+        """
+        sets conversation
+        """
+        self.conversation = conversation
+
+    def talk(self):
+        """
+        for defining a talk
+        """
+        if self.conversation:
+            print(f"[{self.name} говорить]: {self.conversation}")
+        else:
+            print(f"{self.name} не бажає розмовляти.")
+
+class Enemy(Character):
+    """
+    class Enemy for defining your enemies
+    """
+    def __init__(self, name, description):
+        super().__init__(name, description)
+        self.weakness = None
+        self.defeated = 0
+
+    def set_weakness(self, weakness):
+        """
+        for setting weakness
+        """
+        self.weakness = weakness
+
+    def get_weakness(self):
+        """
+        fo getting weakness
+        """
+        return self.weakness
+
+    def get_name(self):
+        """
+        for getting a name
+        """
+        return self.name
+
+    def fight(self, item):
+        """
+        for making a fight with enemy
+        """
+        if item == self.weakness:
+            print(f"Ви боретесь з {self.name} маючи {item}.")
+            self.defeated += 1
+            return True
+        else:
+            print(f"{self.name} б'є Вас!")
+            return False
+
+    def get_defeated(self):
+        """
+        for defeatimg
+        """
+        return self.defeated
+
+class Friend(Character):
+    """
+    class Friend that takes arguments from class Character
+    for defining your friend during game
+    """
+    def __init__(self, name, description):
+        super().__init__(name, description)
+
+class Item:
+    """
+    class Item for difining an item
+    """
+    def __init__(self, name):
+        self.name = name
+        self.description = None
+
+    def set_description(self, description):
+        """
+        for description
+        """
+        self.description = description
+
+    def describe(self):
+        """
+        method for decribing an item in game
+        """
+        print(f"Предмет {self.name}: {self.description}")
+
+    def get_name(self):
+        """
+        fo getting a name
+        """
+        return self.name
--- main.py
+++ main.py
@@ -1,1 +1,122 @@
+"""
+Головний цикл гри "Блукачка по Львову"
+"""
 
+import game
+
+kozelnytska = game.Street("Козельницька")
+kozelnytska.set_description("Студмістечко.\nТут ви можете зустріти студентів\
+ факультету прикладнх наук, які вмирають від дедлайнів і не мають сил \
+з кимось боротися, окрім сну.\nЩоб піти далі Ви вказуєте напрямок, на зустрічі\
+ з ворогом маєте можливість поговорити, боротися та взяти те, що знаходиться.")
+
+stryiska = game.Street("Стрийська")
+stryiska.set_description("Стрийська - найдовша львівська вулиця, але часто тут зустрінеш...")
+
+naukova = game.Street("Наукова")
+naukova.set_description("Наукова - отримала назву \
+від науково-дослідних інститутів і ту можна побачити....")
+
+vygovskoho = game.Street("Івана Виговського")
+vygovskoho.set_description("Одна з найбільгих вулиць Залізничногго райнону і тут...")
+
+horodotska = game.Street("Городоцька")
+horodotska.set_description("Велика вулиця, яка бере початок у центрі, \
+її протяжність 8,5 км, тут зустрінемоооо...")
+
+kozelnytska.link_street(stryiska, "північ")
+stryiska.link_street(naukova, "південь")
+naukova.link_street(vygovskoho, "захід")
+vygovskoho.link_street(horodotska, "схід")
+horodotska.link_street(kozelnytska, 'північ')
+
+lotr = game.Enemy("Лотр", "А це негідник, розбійник, грабіжник")
+lotr.set_conversation("А куди це ми прямуємо, не поговоривши зі мною?")
+lotr.set_weakness("прикраси та гроші")
+stryiska.set_character(lotr)
+
+batyar = game.Enemy("Батяр", "Гульвіса, п'яничка, брутальний чоловік")
+batyar.set_conversation("Зупиняємось тут, перехожий!")
+batyar.set_weakness("львівське пиво")
+naukova.set_character(batyar)
+
+zbuy = game.Enemy("Збуй", "Розбійник, грабіжник.")
+zbuy.set_conversation("ОГО, хто це?!")
+zbuy.set_weakness("дукати")
+vygovskoho.set_character(zbuy)
+
+laidak = game.Enemy("Лайдак", " Нероба, лінива людина.")
+laidak.set_conversation("Стііііій?!")
+laidak.set_weakness("булочка")
+horodotska.set_character(laidak)
+
+dukaty = game.Item("дукати")
+dukaty.set_description("Нові грошики")
+vygovskoho.set_item(dukaty)
+
+roll = game.Item("булочка")
+roll.set_description('Смачна булочка')
+horodotska.set_item(roll)
+
+accessories = game.Item("прикраси та гроші")
+accessories.set_description("Дорогоцінні предмети, що приваблюють")
+stryiska.set_item(accessories)
+
+beer = game.Item("львівське пиво")
+beer.set_description("Улюблене львівське пиво батярів")
+naukova.set_item(beer)
+
+current_room = kozelnytska
+backpack = []
+
+dead = False
+
+while dead == False:
+
+    print("\n")
+    current_room.get_details()
+
+    inhabitant = current_room.get_character()
+    if inhabitant is not None:
+        inhabitant.describe()
+
+    item = current_room.get_item()
+    if item is not None:
+        item.describe()
+
+    command = input("> ")
+
+    if command in ["північ", "південь", "захід", "схід"]:
+        current_room = current_room.move(command)
+    elif command == "поговорити":
+        if inhabitant is not None:
+            inhabitant.talk()
+    elif command == "боротися":
+        if inhabitant is not None:
+            print("Чим будете боротися?")
+            fight_with = input()
+            if fight_with in backpack:
+
+                if inhabitant.fight(fight_with) == True:
+                    print("Урааа, Ви виграли бійку!")
+                    current_room.character = None
+                    if inhabitant.get_defeated() == 2:
+                        print("Вітання, Ви перемогли ворога!")
+                        dead = True
+                else:
+                    print("Не засмучуйтесь, Ви програли.")
+                    print("Кінець гри")
+                    dead = True
+            else:
+                print("Ви не маєте " + fight_with)
+        else:
+            print("Немає з ким битися")
+    elif command == "взяти":
+        if item is not None:
+            print("Ви поклали " + item.get_name() + " у торбу")
+            backpack.append(item.get_name())
+            current_room.set_item(None)
+        else:
+            print("Нема що взяти!")
+    else:
+        print("Що робити з " + command + "?")
